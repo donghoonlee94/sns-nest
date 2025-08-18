0dac4865cc8bd27db97adb48ccc2ead3de4e8fda
donghoon.lee <summarhoony@nurimedia.co.kr>
2025-08-18 17:54:58 +0900

    add: posts modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getPosts(): Post[] {
-    return [
-      {
-        author: 'John Doe',
-        title: 'Hello World',
-        content: 'This is a test post',
-        likeCount: 10,
-        commentCount: 5,
-      },
-    ];
-  }
 }
