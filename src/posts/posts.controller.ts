27cc4ffe95403826f3f904e0dbf85395de6b1ca3
donghoonlee94 <donghoonlee94@smartscore.kr>
2025-08-26 13:55:22 +0900

    feat: add dynamic params API

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 interface Post {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,20 +10,44 @@ interface Post {
   commentCount: number;
 }
 
+const posts: Post[] = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'Hello World',
+    content: 'This is a test post',
+    likeCount: 10,
+    commentCount: 5,
+  },
+  {
+    id: 2,
+    author: 'Jane2',
+    title: 'Hello World 2',
+    content: 'This is a test post 2',
+    likeCount: 20,
+    commentCount: 555,
+  },
+  {
+    id: 3,
+    author: 'John3',
+    title: 'Hello World 3',
+    content: 'This is a test post 3',
+    likeCount: 103,
+    commentCount: 53,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
   getPosts(): Post[] {
-    return [
-      {
-        author: 'John Doe',
-        title: 'Hello World',
-        content: 'This is a test post',
-        likeCount: 10,
-        commentCount: 5,
-      },
-    ];
+    return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: number): Post | undefined {
+    return posts.find((post) => post.id === +id);
   }
 }
