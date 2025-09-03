ee1acd5de7979b39a499675ac9482b6a109dced5
donghoonlee <94donghoon.lee@gmail.com>
2025-09-03 13:32:53 +0900

    feat: add post method

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 interface Post {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,20 +10,67 @@ interface Post {
   commentCount: number;
 }
 
+let posts: Post[] = [
+  {
+    id: 1,
+    author: 'John Doe',
+    title: 'Hello World',
+    content: 'This is a test post',
+    likeCount: 10,
+    commentCount: 5,
+  },
+  {
+    id: 2,
+    author: 'John Doe',
+    title: 'Hello World',
+    content: 'This is a test post',
+    likeCount: 10,
+    commentCount: 5,
+  },
+  {
+    id: 3,
+    author: 'John Doe',
+    title: 'Hello World',
+    content: 'This is a test post',
+    likeCount: 10,
+    commentCount: 5,
+  },
+  {
+    id: 4,
+    author: 'John Doe',
+    title: 'Hello World',
+    content: 'This is a test post',
+    likeCount: 10,
+    commentCount: 5,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
   getPosts(): Post[] {
-    return [
-      {
-        author: 'John Doe',
-        title: 'Hello World',
-        content: 'This is a test post',
-        likeCount: 10,
-        commentCount: 5,
-      },
-    ];
+    return posts;
+  }
+
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts = [...posts, post];
+
+    return post;
   }
 }
